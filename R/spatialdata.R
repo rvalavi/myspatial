ecc6b2ef9c83e603754dd747551a1375e4652e8e
Roozbeh Valavi <valavi.r@gmail.com>
2019-09-05 12:20:44 +1000

    makePoints

---
+++ R/spatialdata.R
@@ -0,0 +1,20 @@
+#' Degitizing points on a spatial plot
+#'
+#' @param sp logical. To return spatial points
+#' @param col the color to plot points with
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+makePoints <- function(sp = TRUE, col = "red", ...){
+  xy <- graphics::locator(n = 10000, type = "p", col = col, ...)
+  xy <- cbind(xy$x, xy$y)
+  if(sp){
+    return(SpatialPoints(xy))
+  }
+  else {
+    return(xy)
+  }
+}
