3e5106b8ee9921e8aa7b643a5885c6497c85e62f
Roozbeh Valavi <valavi.r@gmail.com>
2019-12-24 14:14:34 +1100

    quardatic for glmnet

--- R/prediction_helper.R
+++ R/prediction_helper.R
@@ -1,8 +1,8 @@
-#' Orthogonal polynomial for glmnet
+#' Orthogonal quadratic polynomials for glmnet
 #'
 #' A function to creat quadratic terms for lasso and ridge
 #'
-#' @param train training data.frame
+#' @param df The data.frame of training covariates
 #' @param cols Character. Column names to generate quadratic terms
 #' @param all If TRUE, all the columns are used to generate quadratic terms
 #'
@@ -10,21 +10,26 @@
 #' @export
 #'
 #' @examples
-glmnetPoly <- function(train, cols = NULL, all = TRUE){
+quadratic_for_glmnet <- function(df, cols = NULL, all = TRUE){
   if(is.null(cols)){
     all <- TRUE
-    cols <- colnames(train)
+    cols <- colnames(df)
+  }
+  for(j in cols){
+    if(is.factor(df[,j])){
+      cols <- cols[which(cols != j)]
+      warning("Column '", j , "' is a factor, so it is excluded from quadratic columns.")
+    }
   }
   for(i in cols){
-    trainCol <- which(names(train) == i)
-    # generating quadratic terms for training data
-    xbar <- mean(train[, trainCol])
-    x1 <- train[, trainCol] - xbar
+    dfCol <- which(names(df) == i)
+    xbar <- mean(df[, dfCol])
+    x1 <- df[, dfCol] - xbar
     alpha <- sum(x1 ^ 3) / sum(x1 ^ 2)
     x2 <- x1 ^ 2 - alpha * x1
     myx1 <- data.frame(mx1 = x1, mx2 = x2)
-    colnames(myx1) <- c(paste0(names(train)[trainCol], "1"), paste0(names(train)[trainCol], "2"))
-    train <- cbind(myx1, train[, -trainCol])
+    colnames(myx1) <- c(paste0(names(df)[dfCol], "1"), paste0(names(df)[dfCol], "2"))
+    df <- cbind(myx1, df[, -dfCol])
   }
-  return(train)
+  return(df)
 }
