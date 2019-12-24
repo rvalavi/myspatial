6901b91c67614b32e11506ddce9c295bc1d9e4da
Roozbeh Valavi <valavi.r@gmail.com>
2019-12-24 14:24:30 +1100

    help page

---
+++ R/myspatial.R
@@ -0,0 +1,11 @@
+#' myspatial: A collection of functions for spatial data analysis and modelling
+#'
+#' A collection of functions to facilitate processing spatial data for analysis and modelling.
+#' The package is in its early stage of development, thus the functions provided here come with ABSOLUTELY NO WARRANTY.
+#'
+#'
+#' @name myspatial
+#' @docType package
+#' @author Roozbeh Valavi
+#' @import sf raster ggplot2
+NULL
